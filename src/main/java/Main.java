5cd10c940c6c9fde1f6a6d4261397f082fe4dc55
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-03-05 18:23:25 +0100

    update Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Transaction transaction = new Transaction();
-        transaction.withdrawalFromBalance(2, 15);
+
     }
 }
