749564be0a360a6acd91561b48d497c4dfbeeb1a
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-03-05 20:34:38 +0100

    update User

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -6,6 +6,26 @@ public class User {
     private String name;
     private String address;
 
+    public User(String name) {
+        this.name = name;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            String sqlCommand = "INSERT INTO users(name) VALUES (?)";
+            try (Connection conn = DatabaseConnection.getConnection();
+                 PreparedStatement pstmt = conn.prepareStatement(sqlCommand)) {
+
+                pstmt.setString(1, name);
+                pstmt.executeUpdate();
+
+                System.out.println("User is created");
+            }
+        } catch (Exception ex) {
+            System.out.println("Connection to database failed...");
+            System.out.println(ex);
+        }
+    }
+
     public User(String name, String address) {
         this.name = name;
         this.address = address;
